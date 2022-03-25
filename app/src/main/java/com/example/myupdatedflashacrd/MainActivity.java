3fad71569c3cc8676d246c147e6e5acd26433bc9
ucey-star <96317780+ucey-star@users.noreply.github.com>
2022-03-26 08:39:44 +0900

    lab3

--- app/src/main/java/com/example/myupdatedflashacrd/MainActivity.java
+++ app/src/main/java/com/example/myupdatedflashacrd/MainActivity.java
@@ -7,12 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
+
+    public int getRandomNumber(int minNumber, int maxNumber) {
+        Random rand = new Random();
+        return rand.nextInt((maxNumber - minNumber) + 1) + minNumber;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         findViewById(R.id.stuff).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +53,44 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+
+        findViewById(R.id.nextBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentCardDisplayedIndex += 1;
+
+
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Snackbar.make(view,
+                            "You've reached the end of the cards, going back to start.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                    currentCardDisplayedIndex = 0;
+                }
+
+                Flashcard currentcard = allFlashcards.get(getRandomNumber(0,currentCardDisplayedIndex));
+                ((TextView) findViewById(R.id.stuff)).setText(currentcard.getQuestion());
+                ((TextView) findViewById(R.id.answer)).setText(currentcard.getAnswer());
+
+
+            }
+        });
+
+        findViewById(R.id.deleteBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.stuff)).getText().toString());
+                Snackbar.make(v,
+                        "You've successfully deleted this flashcard.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
     }
 
     @Override
@@ -47,7 +101,16 @@ public class MainActivity extends AppCompatActivity {
             ((TextView) findViewById(R.id.stuff)).setText(string1);
             ((TextView) findViewById(R.id.answer)).setText(string2);
 
+
+            flashcardDatabase.insertCard(new Flashcard(string1,string2 ));
+
+
+
+            allFlashcards = flashcardDatabase.getAllCards();
+
         }
+
+
 
 
     }
